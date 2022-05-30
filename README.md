2a72f35e6d618309924e6ececa7b7150cb4e56e3
Marcos <marcoslamuela02@gmail.com>
2022-05-30 18:19:51 +0200

    Actualización del fichero java 1 y index 1, y modificación del readme.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# documentacion
+# En el archivo de practica01Git tenemos dos clases, una para comprobar si un dato es un número y otro para comprobar si el dato introducido es un número decimal.
+# En el archivo index.html esta toda la documentación de ambas clases y lo que hace cada una.
--- practica01Git.java
+++ practica01Git.java
@@ -2,12 +2,13 @@ package Tema7;
 
 import java.util.Scanner;
 
+/**
+ * @author Marcos Aboy Josa
+ * @since 1.8
+ */
 public class practica01Git {
 	
 	/**
-	 * @author Marcos Aboy Josa
-	 * @since 1.8
-	 * 
 	 * Método para comprobar que el dato que nos ha introducido es un número
 	 * 
 	 * @param sc Objeto de tipo scanner 
@@ -22,6 +23,29 @@ public class practica01Git {
 				System.out.println(instruccion);
 				String s = sc.nextLine();
 				n = Integer.parseInt(s);
+				fin = true;
+			} catch (Exception e) {
+				System.out.println("Formato incorrecto");
+			}
+		} while (!fin);
+		return n;
+	}
+	
+	/**
+	 * Método para comprobar que el dato que nos ha introducido es un número decimal
+	 * 
+	 * @param sc Objeto de tipo scanner 
+	 * @param instruccion Dato introducido por pantalla
+	 * @return Devuelve true si el dato que nos han introducido es un número decimal y false si no lo es
+	 */
+	public static double PideNumeroD(Scanner sc, String instruccion) {
+		boolean fin = false;
+		double n = 0;
+		do {
+			try {
+				System.out.println(instruccion);
+				String s = sc.nextLine();
+				n = Double.parseDouble(s);
 				fin = true;
 			} catch (Exception e) {
 				System.out.println("Formato incorrecto");
